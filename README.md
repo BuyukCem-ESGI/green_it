df76e6609cd9b873a4d8c7913432158b94ae8dba
Jiovany VINCENT <jiovanyvincent.dev@gmail.com>
2022-03-18 13:21:26 +0100

    [INIT] css style

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Indice de Fragilité Numérique - Green IT
-### 5IW1 - Jiovany VINCENT, Cem BUYUK
+### 5IW1 - Groupe 7 - Jiovany VINCENT, Cem BUYUK
+### website: https://indicedefragilitenumerique.herokuapp.com/
+Démarrer le projet:
 
+```docker-compose up -d```
+
+Score du Green IT: (+-)93
--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const express = require('express');
 const {engine} = require('express-handlebars');
 const dotenv = require('dotenv');
 const app = express();
+const path = require('path');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true} ));
 app.use(bodyParser.json())
-// app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('hbs', engine());
 app.set('view engine', 'hbs');
@@ -23,6 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(xss());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.post('/', function (req, res) {
   const city = req.body.city;
